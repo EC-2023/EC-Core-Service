029fc6adbb9843d0c2257e89ffaa76746307ca07
Cozark <longho250702@gmail.com>
2023-03-24 17:37:07 +0700

    fix update

--- src/main/java/src/service/userLevel/UserLevelService.java
+++ src/main/java/src/service/userLevel/UserLevelService.java
@@ -13,6 +13,7 @@ import src.service.UserLevel.Dtos.UserLevelCreateDto;
 import src.service.UserLevel.Dtos.UserLevelDto;
 import src.service.UserLevel.Dtos.UserLevelUpdateDto;
 
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
@@ -46,16 +47,16 @@ public class UserLevelService {
         return CompletableFuture.completedFuture(toDto.map(userlevel, UserLevelDto.class));
     }
 
-    @Async
     public CompletableFuture<UserLevelDto> update(UUID id, UserLevelUpdateDto userlevel) {
         UserLevel existingUserLevel = userlevelRepository.findById(id).orElse(null);
         if (existingUserLevel == null)
             throw new NotFoundException("Unable to find user level!");
+        Date creatAt = existingUserLevel.getCreateAt();
         existingUserLevel = toDto.map(userlevel, UserLevel.class);
         existingUserLevel.setId(id);
+        existingUserLevel.setCreateAt(creatAt);
         return CompletableFuture.completedFuture(toDto.map(userlevelRepository.save(existingUserLevel), UserLevelDto.class));
     }
-
     @Async
     public CompletableFuture<Void> remove(UUID id) {
         UserLevel existingUserLevel = userlevelRepository.findById(id).orElse(null);
