62fc5004f93c6675656f7b5ce814d194aa82ca99
Cozark <longho250702@gmail.com>
2023-03-30 15:04:50 +0700

    Fix User Profile Dto

--- src/main/java/src/service/User/Dtos/UserProfileDto.java
+++ src/main/java/src/service/User/Dtos/UserProfileDto.java
@@ -1,66 +1,11 @@
 package src.service.User.Dtos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
 import java.util.UUID;
-
+@Data
 public class UserProfileDto {
-    public UUID getId() {
-        return Id;
-    }
-
-    public void setId(UUID id) {
-        Id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getMiddleName() {
-        return middleName;
-    }
-
-    public void setMiddleName(String middleName) {
-        this.middleName = middleName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
-
-    public void setDisplayName(String displayName) {
-        this.displayName = displayName;
-    }
-
-    public String getLastLogin() {
-        return lastLogin;
-    }
-
-    public void setLastLogin(String lastLogin) {
-        this.lastLogin = lastLogin;
-    }
-
 
     @JsonProperty(value = "Id", required = true)
     public UUID Id;
